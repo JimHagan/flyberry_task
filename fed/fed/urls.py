800ad8a3b75ee27184af8144bdaaf930bc9e39e2
Jim <jim.locately@gmail.com>
2015-03-08 15:43:56 -0400

    Added to index.html

--- fed/fed/urls.py
+++ fed/fed/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import patterns, include, url
 #from django.contrib import admin                                                                                                                   m django.conf.urls import handler404, handler500
 from fomc import views
+from views import index_page
 
 urlpatterns = patterns('',
     #url(r'^admin/', include(admin.site.urls)),
+    #url(r'^$', static_serve, kwargs={
+    #        'path': 'index.html', 'document_root': settings.STATIC_URL}),
+    url(r'^$', index_page),
     url(r'^fed/fomc/', include('fomc.urls'))
 )
 
